17c2e20a513f763c9b6e0707dd06edded2a2af86
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-06 05:03:41 -0700

    Add another class which implements from Coach Master Class

--- src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -3,9 +3,11 @@ package com.luv2code.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach  implements Coach{
+public class TennisCoach implements Coach{
+
+
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15min :-)";
+        return "Practice Your Backhand volley";
     }
 }
