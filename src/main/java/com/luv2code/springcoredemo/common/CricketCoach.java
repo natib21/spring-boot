133dddc520a36ec54cdea11f2a77ff9c8ca34069
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-09 07:47:48 -0700

    Java Config With Help Of @Bean and @Config Annotations

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,7 @@
 package com.luv2code.springcoredemo.common;
 
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,14 +14,7 @@ public class CricketCoach  implements Coach{
     public CricketCoach(){
         System.out.println("In Constructor " + getClass().getSimpleName());
     }
-    @PostConstruct
-    public void doMyStartupStuff(){
-        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
-    }
-    @PreDestroy
-    public void doMycleanupStuff(){
-        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
-    }
+
 
 
     @Override
